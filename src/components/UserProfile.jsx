fe1b0fa491dbe2c119fc844c7f424a5e89a50591
Edga380 <a.edgaras44@gmail.com>
2024-03-23 00:34:54 +0000

    User profile update

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,7 +6,16 @@ export const UserProfile = () => {
       <div className="section-name">PROFILE</div>
       <div className="profile-section-container">
         <div className="profile-bg">
-          <div className="profile-information-container"></div>
+          <div className="profile-information-container">
+            <div className="profile-avatar-container">
+              <img
+                className="profile-avatar"
+                src="./public/default_avatar.svg"
+                alt=""
+              />
+              <button className="change-profile-avatar-button">S</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
